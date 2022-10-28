b329feaf70093c079ecb35535ddda0eee39b01fa
Jesus C. Compaire <108104717+jcompaire@users.noreply.github.com>
2022-10-28 14:51:31 -0300

    Update README.m

--- HYCOM_download_netCDF/README.m
+++ HYCOM_download_netCDF/README.m
@@ -1,3 +1,3 @@
 % A MATLAB script to download hourly HYCOM data via OpenDAP and store them as ncfiles
-% separately (get_hycom_individual) or combined into a single one (get_hycom_whole).
+% separately (get_hycom_individual.m) or combined into a single one (get_hycom_whole.m).
 % Source of HYCOM data: https://www.hycom.org/data/goml0pt04/expt-32pt5
